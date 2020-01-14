3594703ece60f400f6ac4cb71be7d6b34fd9ea5a
Christopher Chang <chiggiechang@gmail.com>
2020-01-14 20:52:44 +0000

    Add lines to a file

--- helloworld2.cpp
+++ helloworld2.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
     cout << "Hello, new file!" << endl;
+    cout << "I had to add this statement because Rene said so." << endl;
+    cout << "Adding another cout." << endl;
     return 0;
 }
